48140c5ec4eab592664541ea7689de4dee22720c
Tonye Greene <35413072+ellysmoore@users.noreply.github.com>
2024-04-17 15:41:28 +0100

    trial 12

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cowas-shop/',
+  base: 'http://cowas-shop/',
   server: {
     fs: {
       allow: ['..']
