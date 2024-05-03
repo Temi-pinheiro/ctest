b065f9878f7683d68e2c84b3758549cbef971cb5
Tonye Greene <35413072+ellysmoore@users.noreply.github.com>
2024-05-03 05:45:33 +0100

    role check for wallet

--- src/pages/account-pages/Account.tsx
+++ src/pages/account-pages/Account.tsx
@@ -1,14 +1,14 @@
-import { useAuth } from '../../providers';
+import { useAuth } from "../../providers";
 
 export const AccountPage = () => {
   const { user } = useAuth();
   return (
-    <div className='w-full flex flex-col text-[#2C2844]'>
-      <h1 className='text-2xl font-semibold pb-6'>Account Overview</h1>
-      <div className='w-full flex flex-col gap-y-10'>
-        <div className='border-t pt-10'>
-          <h4 className='text-xl font-medium'>Account Details</h4>
-          <div className=' flex flex-col mt-5 gap-y-[10px]'>
+    <div className="w-full flex flex-col text-[#2C2844]">
+      <h1 className="text-2xl font-semibold pb-6">Account Overview</h1>
+      <div className="w-full flex flex-col gap-y-10">
+        <div className="border-t pt-10">
+          <h4 className="text-xl font-medium">Account Details</h4>
+          <div className=" flex flex-col mt-5 gap-y-[10px]">
             <span>
               {user.firstname} {user.lastname} ({user.email})
             </span>
@@ -16,15 +16,18 @@ export const AccountPage = () => {
             <span>{user.phone_number}</span>
           </div>
         </div>
-        <div className='border-t pt-10'>
-          <h4 className='text-xl font-medium'>Wallet Credit</h4>
-          <div className=' flex flex-col mt-5 gap-y-[10px]'>
-            <span>Wallet Balance: 12,000.00</span>
+        {user.role.title !== "Retailer" && (
+          <div className="border-t pt-10">
+            <h4 className="text-xl font-medium">Wallet Credit</h4>
+            <div className=" flex flex-col mt-5 gap-y-[10px]">
+              <span>Wallet Balance: ₦12,000.00</span>
+            </div>
           </div>
-        </div>
-        <div className='border-t pt-10'>
-          <h4 className='text-xl font-medium'>Address</h4>
-          <div className=' flex flex-col mt-5 gap-y-[10px]'>
+        )}
+
+        <div className="border-t pt-10">
+          <h4 className="text-xl font-medium">Address</h4>
+          <div className=" flex flex-col mt-5 gap-y-[10px]">
             Your default shipping address:
             <br />
             <span>
